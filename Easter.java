cc9337560748ccc96f0801973439a334c10a6f31
coolight543 <robamiao@gmail.com>
2025-09-03 18:18:46 +0000

    initial progress 9/3

--- Easter.java
+++ Easter.java
@@ -1,1 +1,10 @@
+// Kuang Miao
+// This program will calculate the date of Easter in each years
+public class Easter{
+    public static void main(string[] args){
+        // code goes here
+        
 
+    }
+
+}
