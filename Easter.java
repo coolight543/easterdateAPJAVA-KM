9473756b967effb2dbc0d8c00229b8c5a1eea5c2
coolight543 <robamiao@gmail.com>
2025-09-04 18:15:07 +0000

    done1

--- Easter.java
+++ Easter.java
@@ -3,8 +3,21 @@
 public class Easter{
     public static void main(string[] args){
         // code goes here
-        
+        int y = 2003;
+        double a = y%19;
+        int b = y/100;
+        int c = y%100;
+        int d = b/4;
+        int e = b%4;
+        int f = (b+8)/25;
+        int g = (b-f+1)/3;
+        int h = (19*a+b-d-g+15)/30;
+        int i = c/4
+        int k = c%4
+        int r = 32 + 
 
+        
+    
     }
 
 }
